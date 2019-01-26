149a07388e33c47217b2622022ba472b18cdc161
Dennis Lieu <dennis.lieu@innogames.com>
2019-01-27 00:06:06 +0100

    Add RingCanvas prefab

---
+++ Assets/ResourceProgressBar.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceProgressBar : MonoBehaviour
+{
+    public Image ProgressBar;
+    private int _maxSegmentScore;
+
+    private void Start()
+    {
+        Game.Instance.GameModel.OnScoreChange += OnScoreChanged;
+        var segmentScores = Game.Instance.GameSettings.segmentScores;
+        _maxSegmentScore = segmentScores[segmentScores.Length - 1];
+    }
+
+    private void OnScoreChanged(int score)
+    {
+        ProgressBar.fillAmount = (float) score / _maxSegmentScore;
+    }
+}
