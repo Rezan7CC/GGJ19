44da7b325e5c8f627dcdda9b56abe0edf9fec436
Dennis Lieu <dennis.lieu@innogames.com>
2019-01-27 01:18:35 +0100

    Add camera zoom for shield mode

---
+++ Assets/CameraBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public float ZoomSize = 4.5f;
+    private float originalSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalSize = Camera.main.orthographicSize;
+
+        Game.Instance.GameModel.OnControlModeChanged += OnControlModeChange;
+    }
+
+    private void OnControlModeChange(ControlMode controlmode)
+    {
+        if (controlmode == ControlMode.ShieldMovement)
+        {
+            Camera.main.DOOrthoSize(ZoomSize, 0.25f);
+        }
+        else
+        {
+            Camera.main.DOOrthoSize(originalSize, 0.25f);
+        }
+    }
+}
