9a3528a9e905f0a3083a4f636867f3cd483de2e0
Dennis Lieu <dennis.lieu@innogames.com>
2019-01-27 01:50:41 +0100

    Emit particle for up arrow as well

--- Assets/Scripts/particleTrail.cs
+++ Assets/Scripts/particleTrail.cs
@@ -8,7 +8,7 @@ public class particleTrail : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W) && !(Game.Instance.GameModel.GetControlMode() == ControlMode.ResourceGathering) 
+        if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) && !(Game.Instance.GameModel.GetControlMode() == ControlMode.ResourceGathering) 
             && !(Game.Instance.GameModel.GetControlMode() == ControlMode.ShieldMovement))
         {
             thruster.Emit(particleAmount); //Emit some particle
