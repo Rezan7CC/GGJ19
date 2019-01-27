412a9a6baf0eca1d7021cdd6a4d390b263562f04
Dennis Lieu <dennis.lieu@innogames.com>
2019-01-27 14:34:07 +0100

    Prepare flying names prefab for credits

---
+++ Assets/Scripts/FlyingNames.cs
@@ -0,0 +1,24 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlyingNames : MonoBehaviour
+{
+    public Image[] names;
+    public AnimationCurve xMovementCurve;
+    public float delay;
+    public int distance;
+    public int animationDuration;
+    
+    void Start()
+    {
+        for (var i = 0; i < names.Length; i++)
+        {
+            var image = names[i];
+            image.transform
+                .DOMoveX(image.transform.position.x + distance, animationDuration)
+                .SetDelay(i * delay)
+                .SetEase(xMovementCurve);
+        }
+    }
+}
